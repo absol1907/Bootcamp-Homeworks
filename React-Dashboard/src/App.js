ce3007cfd7ed2d8b9216f7507f0940f114915154
absol1907 <97902913+absol1907@users.noreply.github.com>
2022-07-20 16:19:30 +0300

    Added Third Homework

---
+++ React-Dashboard/src/App.js
@@ -0,0 +1,12 @@
+import Projects from "./components/data/project";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Dashboard</h1>
+      <Projects></Projects>
+    </div>
+  );
+}
+
+export default App;
